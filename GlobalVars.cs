9b27ce43dc67e2460d2774e3d48b528dfa97eb18
Nagisa_Cheese <kotori@tongji.edu.cn>
2020-11-29 14:37:36 +0800

    API gateway services
    API 网关服务

---
+++ GlobalVars.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Configuration;
+
+namespace APIGateways
+{
+    public static class GlobalVars
+    {
+        // You need to add app.config to the projest's directory manually
+        public static readonly string secret = ConfigurationManager.AppSettings["Secret"].ToString();
+        public static readonly string domain = ConfigurationManager.AppSettings["Domain"].ToString();
+        public static readonly int listenPort = Convert.ToInt32(ConfigurationManager.AppSettings["Domain"]);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,10 +2,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 
 namespace APIGateways
 {
@@ -20,6 +18,27 @@ namespace APIGateways
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder
+                    .UseKestrel(o =>
+                    {
+                        o.Listen(IPAddress.Any, GlobalVars.listenPort);
+                    })
+                    .UseUrls($"http://localhost:{GlobalVars.listenPort}")
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .ConfigureAppConfiguration((hostingContext, config) =>
+                    {
+                        config
+                            .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                            .AddJsonFile("appsettings.json", true, true)
+                            .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                            .AddJsonFile("ocelot.json", false, true)
+                            .AddEnvironmentVariables();
+                    })
+                    .ConfigureLogging((hostingContext, logging) =>
+                    {
+                        logging.ClearProviders();
+                        logging.AddConsole();
+                    });
                     webBuilder.UseStartup<Startup>();
                 });
     }
